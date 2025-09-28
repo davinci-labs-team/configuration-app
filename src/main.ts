9b00e680bb4bfe7a10a55fa3c5714f99c69210a2
Hadrien Casadesus <hadrien.casa@outlook.fr>
2025-09-28 15:54:15 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,22 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 const vuetify = createVuetify({
-  components,
-  directives,
-})
-const app = createApp(App).use(vuetify)
+    components,
+    directives
+});
+const app = createApp(App).use(vuetify);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
