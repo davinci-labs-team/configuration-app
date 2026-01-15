17ab5d639e9a5eccb9bc007f47fd20cfe444c263
Hadrien Casadesus <hadrien.casa@outlook.fr>
2026-01-15 11:07:50 +0100

    refactor: update package.json and vite.config.ts for improved configuration
    - Simplified scripts in package.json by removing unnecessary commands.
    - Updated dependencies in package.json, including a minor version change for vite-plugin-vue-devtools.
    - Refactored vite.config.ts to conditionally include vueDevTools only in development mode and improved code formatting.

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
+    plugins: [
+        vue(),
+        process.env.NODE_ENV === "development" ? vueDevTools() : null,
+        tailwindcss()
+    ].filter(Boolean),
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url))
+        }
+    }
+});
