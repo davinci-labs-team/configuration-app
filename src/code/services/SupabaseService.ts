acee23ca2da90742d4a3459aaf5065b9a9c6412e
Hadrien Casadesus <hadrien.casa@outlook.fr>
2026-01-27 11:39:19 +0100

    refactor: update createAdminUser method to use signUp for user registration

--- src/code/services/SupabaseService.ts
+++ src/code/services/SupabaseService.ts
@@ -37,9 +37,8 @@ export default abstract class SupabaseService {
     public static async createAdminUser(email: string, password: string): Promise<boolean> {
         const supabaseStore = useSupabaseStore();
         const supabaseClient = supabaseStore.client!;
-        const { data, error } = await supabaseClient.auth.admin.createUser({
+        const { data, error } = await supabaseClient.auth.signUp({
             email: email,
-            email_confirm: true,
             password: password
         });
         if (error) {
